f92ff4226df29fe806873caafd4366756d6bbe97
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-27 15:03:35 -0700

    moved APP to own component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from './components/search_bar';
-
-// Youtube API key
-const API_KEY = 'AIzaSyBWQfANXbtQFADgHD5ilToR6mJrl-SQmwQ';
-
-// Create new component, should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-}
-
-// Take this componenet's generated HTML and put it on the page
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
